03bfc7ae69c636a5c02a81479d116c583e0d0c9d
Kirtesh Bharambe <kirteshbharambe0410@gmail.com>
2026-09-23 05:20:11 +0530

    Create Program16.cpp

---
+++ Program16.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+#include <utility>
+
+class Employee {
+protected:
+    int employeeId;
+    std::string name;
+
+public:
+    Employee(int id, std::string employeeName)
+        : employeeId(id), name(std::move(employeeName)) {}
+
+    virtual double calculateSalary() const = 0;
+
+    void displayBasicDetails() const {
+        std::cout << "Employee ID: " << employeeId << '\n';
+        std::cout << "Name: " << name << '\n';
+    }
+
+    virtual ~Employee() = default;
+};
+
+class PermanentEmployee : public Employee {
+private:
+    double basicSalary;
+    double allowance;
+
+public:
+    PermanentEmployee(int id, std::string employeeName, double basic, double extra)
+        : Employee(id, std::move(employeeName)),
+          basicSalary(basic), allowance(extra) {}
+
+    double calculateSalary() const override {
+        return basicSalary + allowance;
+    }
+};
+
+class ContractEmployee : public Employee {
+private:
+    double hourlyRate;
+    int hoursWorked;
+
+public:
+    ContractEmployee(int id, std::string employeeName, double rate, int hours)
+        : Employee(id, std::move(employeeName)),
+          hourlyRate(rate), hoursWorked(hours) {}
+
+    double calculateSalary() const override {
+        return hourlyRate * hoursWorked;
+    }
+};
+
+void printPaySlip(const Employee& employee) {
+    employee.displayBasicDetails();
+    std::cout << "Salary: Rs. " << employee.calculateSalary() << "\n\n";
+}
+
+int main() {
+    PermanentEmployee permanentEmployee(201, "Meera", 45000.0, 7500.0);
+    ContractEmployee contractEmployee(202, "Rohan", 600.0, 75);
+
+    printPaySlip(permanentEmployee);
+    printPaySlip(contractEmployee);
+
+    return 0;
+}
