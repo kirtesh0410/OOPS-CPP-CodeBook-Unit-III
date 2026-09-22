f33b0542f36b7ce4f8ef4b87c5051c223df3ffbe
Kirtesh Bharambe <kirteshbharambe0410@gmail.com>
2026-09-23 05:12:55 +0530

    Create Program06.cpp

---
+++ Program06.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+class Distance {
+private:
+    int meters;
+
+public:
+    explicit Distance(int value) : meters(value) {}
+
+    bool operator>(const Distance& other) const {
+        return meters > other.meters;
+    }
+
+    void display() const {
+        std::cout << meters << " meters\n";
+    }
+};
+
+int main() {
+    Distance first(150);
+    Distance second(120);
+
+    std::cout << "First distance: ";
+    first.display();
+
+    std::cout << "Second distance: ";
+    second.display();
+
+    if (first > second) {
+        std::cout << "First distance is greater\n";
+    } else {
+        std::cout << "Second distance is greater or equal\n";
+    }
+
+    return 0;
+}
